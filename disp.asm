81d2b192b8fa9dc3523c343efa28ba5c5b665ad5
ssr123-ssr <1557177832@qq.com>
2022-11-09 15:03:54 +0800

    第四次作业

---
+++ disp.asm
@@ -0,0 +1,45 @@
+
+PUBLIC DISP
+
+CODE SEGMENT para 'code'
+	ASSUME CS:CODE
+DISP PROC FAR
+	PUSH DX
+	PUSH AX
+	PUSH BP
+	MOV BP,SP
+
+	
+	MOV AX,DX
+    XOR DX,DX
+    MOV BX,10
+    MOV CX,0
+A:
+    CMP AX,10
+    JB OK
+    DIV BX
+    ADD DL,30H
+    PUSH DX
+    XOR DX,DX
+    INC CX
+    JMP A
+OK:
+    ADD AL,30H
+    PUSH AX
+    INC CX
+B:
+    POP DX
+    MOV AH,2
+    INT 21H
+    LOOP b
+	
+	POP DX
+	POP AX
+	POP BP
+	RETF
+DISP ENDP
+
+CODE ENDS
+END
+
+
